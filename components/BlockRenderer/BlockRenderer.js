4854823006fdd3f28ee20ea3c8662604229a5fbf
“Glecio <glecio.santos@gmail.com>
2023-09-27 18:18:56 -0300

    images and heading style

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,8 +1,22 @@
+import { Cover } from "components/Cover"
+import { Heading } from "components/Heading"
+
 export const BlockRenderer = ({blocks}) => {
     return blocks.map(block => {
         switch(block.name){
+            case "core/heading" :{
+                return <Heading key={block.id} 
+                textAlign={block.attributes.textAlign} 
+                level={block.attributes.level}
+                content={block.attributes.content}
+                />
+            }
             case "core/cover" : {
-                return <div key={block.id}> core cover </div>
+                return (
+                    <Cover key={block.id} background={block.attributes.url}> 
+                        <BlockRenderer blocks={block.innerBlocks} />
+                    </Cover>
+                )
             }
             default: 
             return null
