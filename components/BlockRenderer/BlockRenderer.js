72cdcdd536a442118987c925d91f742ea642c886
“Glecio <glecio.santos@gmail.com>
2023-10-22 09:18:56 -0300

    Call to Action as Next Component

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,3 +1,4 @@
+import { CallToActionButton } from "components/CallToActionButton/CallToActionButton"
 import { Cover } from "components/Cover"
 import { Heading } from "components/Heading"
 import { Paragraph } from "components/Paragraph/Paragraph"
@@ -6,6 +7,10 @@ import { theme } from "theme"
 export const BlockRenderer = ({blocks}) => {
     return blocks.map(block => {
         switch(block.name){
+            case "acf/ctabutton" :{
+                return <CallToActionButton key={block.id} buttonLabel={block.attributes.data.label} destination={block.attributes.data.destination} align={block.attributes.data.align} />
+            }
+
             case "core/paragraph" :{
                 return <Paragraph key={block.id} 
                 textAlign={block.attributes.align} 
@@ -30,8 +35,10 @@ export const BlockRenderer = ({blocks}) => {
                     </Cover>
                 )
             }
-            default: 
-            return null
+            default: {
+                console.log("blockrender:", block)
+                return null
+            }
         }
     })
 }
